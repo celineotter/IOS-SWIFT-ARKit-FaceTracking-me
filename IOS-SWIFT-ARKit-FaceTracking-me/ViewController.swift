d2f8c918c582249807cf1baccf287efa7cb1b99a
Celine Otter <cotter@akiliinteractive.com>
2020-04-06 14:18:23 -0700

    Initial Commit

---
+++ IOS-SWIFT-ARKit-FaceTracking-me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS-SWIFT-ARKit-FaceTracking-me
+//
+//  Created by Celine Otter on 4/6/20.
+//  Copyright © 2020 Celine Otter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOS-SWIFT-ARKit-FaceTracking-meTests/IOS_SWIFT_ARKit_FaceTracking_meTests.swift
@@ -0,0 +1,34 @@
+//
+//  IOS_SWIFT_ARKit_FaceTracking_meTests.swift
+//  IOS-SWIFT-ARKit-FaceTracking-meTests
+//
+//  Created by Celine Otter on 4/6/20.
+//  Copyright © 2020 Celine Otter. All rights reserved.
+//
+
+import XCTest
+@testable import IOS_SWIFT_ARKit_FaceTracking_me
+
+class IOS_SWIFT_ARKit_FaceTracking_meTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
